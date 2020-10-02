90d9d58b6f83220651e08fb8bfb97d27813e7915
soumen maity <soumen.infimonk@gmail.com>
2020-10-02 11:59:52 +0530

    Updates

---
+++ precache-manifest.1783dc2cbd03d598069edcf972bb431c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989abaeaf695b64f0c609ad7fb85d110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f54304cca8c075bdb9",
+    "url": "/static/css/main.163b1cc9.chunk.css"
+  },
+  {
+    "revision": "95c2f0ae90888b2c7138",
+    "url": "/static/js/2.b62b6a19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b62b6a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f54304cca8c075bdb9",
+    "url": "/static/js/main.4a536421.chunk.js"
+  },
+  {
+    "revision": "d3dd96e1d661cf4b63de",
+    "url": "/static/js/runtime-main.02157185.js"
+  },
+  {
+    "revision": "d1bf3ed8e8f71e8b916d705cb624ae39",
+    "url": "/static/media/LoginBanner.d1bf3ed8.jpg"
+  },
+  {
+    "revision": "0ae92f8d8fc889096091798a274e43db",
+    "url": "/static/media/logo.0ae92f8d.png"
+  }
+]);
