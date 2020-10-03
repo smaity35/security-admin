7d79bce003554c0ea8416458d4b3044aa8fa8cfd
soumen maity <soumen.infimonk@gmail.com>
2020-10-04 03:20:33 +0530

    ui update

--- src/App.js
+++ src/App.js
@@ -14,11 +14,10 @@ function App() {
     <>
       <HashRouter basename='/'>
         <Route path="/" exact ><LoginPageComponent /></Route>
-        <Route path="/login"  ><LoginPageComponent /></Route>
-        <Route path="/home"  ><HomePageComponent /></Route>
+        <Route path="/login"  exact ><LoginPageComponent /></Route>
+        <Route path="/home" exact ><HomePageComponent /></Route>
 
-        <Route path="*"  ><LoginPageComponent /></Route>
-      </HashRouter>
+       </HashRouter>
     </>
   );
 }
